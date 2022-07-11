8e1fcf0e2b88967a5f718ddff3f3145518b4890e
yelizaveta-aliantsevich <souslayk@gmail.com>
2022-07-11 11:46:17 +0300

    fixed token

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export default (req, res, next) => {
   try {
-    const token = req.headers.authorization;
+    const token = (req.headers.authorization || '').split(' ')[1];
 
     if (!token) {
       return res.status(403).json({ message: 'Authorization error' });
